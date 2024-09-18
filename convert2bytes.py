02b3271a09f793864a913739a1ea13a79b68f24f
Zou Xunlong <zoux@i2r.a-star.edu.sg>
2024-09-18 22:58:15 +0800

    .

--- convert2bytes.py
+++ convert2bytes.py
@@ -4,7 +4,6 @@ import tempfile
 import fire
 import soundfile as sf
 import os
-import shutil
 
 def get_all_split(root_hf):
     directories = []
@@ -18,14 +17,17 @@ def map_fn(example):
     fname=tempfile.NamedTemporaryFile(suffix=".opus").name
     sf.write(fname, audio_array, 16000, format='OGG', subtype='OPUS')
     example["context"]["audio"]={"bytes": open(fname, "rb").read()}
+
+    if example["answer"]["audio"]:
+        answer_array=example["answer"]["audio"]["array"]
+        fname=tempfile.NamedTemporaryFile(suffix=".opus").name
+        sf.write(fname, answer_array, 16000, format='OGG', subtype='OPUS')
+        example["answer"]["audio"]={"bytes": open(fname, "rb").read()}
+
     return example
 
 def convert(split, num_proc):
-    try:
-        ds=load_from_disk(split)
-    except:
-        print("========================error loading {}=======================================".format(split), flush=True)
-        return
+    ds=load_from_disk(split)
     ds=ds.map(map_fn, num_proc=num_proc, batch_size=1, writer_batch_size=1, features=ds.features)
     ds.save_to_disk(split.replace("datasets_multimodal", "datasets_multimodal_opus_bytes"), num_proc=4)
 
@@ -36,12 +38,11 @@ def main(dir, num_proc=128):
     for split in splits:
         print("=====start {}==========".format(split), flush=True)
         if os.path.exists(split.replace("datasets_multimodal", "datasets_multimodal_opus_bytes")):
-            print("complete {}".format(split), flush=True)
+            print("skip {}".format(split), flush=True)
             continue
         convert(split, num_proc)
         print("======complete {}=======".format(split), flush=True)
 
 if __name__ == "__main__":
     fire.Fire(main)
-    
-    
+
