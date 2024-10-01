2d3c1cc2f5615cc1c4a19f70fcfa78e2a36ef040
Zou Xunlong <zoux@i2r.a-star.edu.sg>
2024-10-01 21:14:50 +0800

    .

--- convert/hf_array2hf_bytes.py
+++ convert/hf_array2hf_bytes.py
@@ -23,25 +23,24 @@ def map_fn(example):
         fname=tempfile.NamedTemporaryFile(suffix=".opus").name
         sf.write(fname, answer_array, 16000, format='OGG', subtype='OPUS')
         example["answer"]["audio"]={"bytes": open(fname, "rb").read()}
-
     return example
 
-def convert(split, num_proc):
+def convert(split, num_proc, dir):
     ds=load_from_disk(split)
     ds=ds.map(map_fn, num_proc=num_proc, batch_size=1, writer_batch_size=1, features=ds.features)
-    if not os.path.exists(split.replace("datasets_multimodal", "datasets_multimodal_opus")):
-        ds.save_to_disk(split.replace("datasets_multimodal", "datasets_multimodal_opus"), num_proc=4)
+    if not os.path.exists(split.replace(dir, dir+"_opus")):
+        ds.save_to_disk(split.replace(dir, dir+"_opus"), num_proc=4)
 
-def main(dir, reverse, num_proc=28):
+def main(dir, reverse=True, num_proc=192):
     splits=get_all_split(dir)
     splits.sort(reverse=reverse)
     pprint(splits)
     for split in splits:
         print("=====start {}==========".format(split), flush=True)
-        if os.path.exists(split.replace("datasets_multimodal", "datasets_multimodal_opus")):
+        if os.path.exists(split.replace(dir, dir+"_opus")):
             print("skip {}".format(split), flush=True)
             continue
-        convert(split, num_proc)
+        convert(split, num_proc, dir)
         print("======complete {}=======".format(split), flush=True)
 
 if __name__ == "__main__":
--- llm_generation/vllm_host.sh
+++ llm_generation/vllm_host.sh
@@ -5,7 +5,7 @@ PORT=$2
 export CUDA_VISIBLE_DEVICES=$GPU
 
 python -m vllm.entrypoints.openai.api_server \
-        --model hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4 \
+        --model /scratch/users/astar/ares/zoux/workspaces/models/Meta-Llama-3.1-8B-Instruct \
         --max-model-len 4096 \
         --port $PORT \
         --disable-log-requests \
