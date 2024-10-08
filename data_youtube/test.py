322f148bb76cfe16ed91bbacc70346b103182921
Xunlong Zou <e0696868@u.nus.edu>
2024-10-08 11:49:54 +0800

    .

--- data_youtube/test.py
+++ data_youtube/test.py
@@ -1,18 +1,26 @@
 from pytube import YouTube
 import os
 
-try:
-    directory="./"
-    filename="5GfKhwJ8FiI.wav"
-    yt = YouTube("https://www.youtube.com/watch?v=5GfKhwJ8FiI")
-    print("youtubeed", flush=True)
-    stream = yt.streams.filter(only_audio=True).first()
-    print("streammed", flush=True)
-    stream.download(directory, filename=filename)
-    print("downloaded", flush=True)
-except Exception as e:
-    if "Forbidden" in str(e):
-        os.remove(os.path.join(directory, filename))
-        print("{} ERROR: {}".format(filename, e), flush=True)
-    else:
-        print("{} ERROR: {}".format(filename, e), flush=True)
+
+
+def down(id):
+    try:
+        directory="./"
+        filename=id+".wav"
+        yt = YouTube("https://www.youtube.com/watch?v="+id)
+        print("youtubeed", flush=True)
+        stream = yt.streams.filter(only_audio=True).first()
+        print("streammed", flush=True)
+        stream.download(directory, filename=filename)
+        print("downloaded", flush=True)
+    except Exception as e:
+        if "Forbidden" in str(e):
+            os.remove(os.path.join(directory, filename))
+            print("{} ERROR: {}".format(filename, e), flush=True)
+        else:
+            print("{} ERROR: {}".format(filename, e), flush=True)
+
+
+if __name__ == "__main__":
+    import fire
+    fire.Fire(down)
