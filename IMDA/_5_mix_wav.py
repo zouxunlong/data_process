cc34c8ac32e221c3278b78d95ff77f77d65ec862
Zou Xunlong <zoux@i2r.a-star.edu.sg>
2025-01-28 11:45:00 +0800

    。

--- IMDA/_5_mix_wav.py
+++ IMDA/_5_mix_wav.py
@@ -1,14 +1,13 @@
+from collections import defaultdict
 from glob import glob
 from itertools import groupby
 import os
 import json
-import librosa
 import numpy as np
 from fire import Fire
 import logging
 import soundfile as sf
 from multiprocessing import Pool
-
 from tqdm import tqdm
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -17,62 +16,36 @@ def get_key(path):
     elements = path.split("/")[-1].replace("-", "_").split("_")
     return elements[1]+"_"+elements[-1]
 
+
 def mix_wav(params):
-    input_files, output_file = params
-    
-    array1, sr1 = sf.read(input_files[0])
-    array2, sr2 = sf.read(input_files[1])
-    if sr1 != 16000:
-        print("start resample {}".format(input_files[0].split("/")[-1]), flush=True)
-        array = librosa.resample(array1, orig_sr=sr1, target_sr=16000, res_type="kaiser_best")
-        sf.write(input_files[0], array, 16000)
-        print("complete resample {}".format(input_files[0].split("/")[-1]), flush=True)
-
-    if sr2 != 16000:
-        print("start resample {}".format(input_files[1].split("/")[-1]), flush=True)
-        array = librosa.resample(array2, orig_sr=sr2, target_sr=16000, res_type="kaiser_best")
-        sf.write(input_files[1], array, 16000)
-        print("complete resample {}".format(input_files[1].split("/")[-1]), flush=True)
+    input_files, output_file, same_time = params
 
     array1, sr1 = sf.read(input_files[0])
     array2, sr2 = sf.read(input_files[1])
 
-    assert sr1 == 16000, f"sr1:{sr1}, {input_files[0]}"
-    assert sr2 == 16000, f"sr2:{sr2}, {input_files[1]}"
+    assert sr1 == sr2 == 16000, f"{input_files[0]}:{sr1}, {input_files[1]}:{sr2}"
 
-    length=min(len(array1), len(array2))
-    sf.write(output_file, array1[0:length] + array2[0:length], sr2)
+    if same_time=="same_time":
+        length = min(len(array1), len(array2))
+        sf.write(output_file, array1[0:length] + array2[0:length], 16000)
+        return (input_files[0], input_files[1], 0, 0)
+    if same_time=="diff_time":
+        length_diff = len(array1) - len(array2)
+        max_agv_sum = 0
 
+        for shift in range(min(-160000, -160000+length_diff), max(160000, length_diff + 160000), 320):
+            mixed_array        = array1[max(0, shift):min(len(array1), len(array2) + shift)] + array2[max(0, -shift):min(len(array1)-shift, len(array2))]
+            mixed_array_length = len(mixed_array)
 
-def mix_wav_with_shift(params):
-    input_files, output_file = params
+            shifted_abs_sum = np.sum(np.absolute(mixed_array))
+            shifted_avg_sum = shifted_abs_sum / mixed_array_length
 
-    array1, sr1 = sf.read(input_files[0])
-    array2, sr2 = sf.read(input_files[1])
-    assert sr1 == sr2, f"sr1:{sr1}, sr2:{sr2}"
-    assert sr1 == 16000, f"sr1:{sr1}, sr2:{sr2}"
-
-    (file_large, array_large), (file_small, array_small) = sorted([(input_files[0].split("/")[-1], array1), (input_files[1].split("/")[-1], array2)], key=lambda x: len(x[1]), reverse=True)
-
-    max_abs_sum = 0
-    length_diff = len(array_large) - len(array_small)
-
-    if length_diff > 160000:
-        return
-
-    for shift in range(-80000, length_diff+80001, 500):
-        mixed_array = np.concatenate((np.zeros(max(-shift, 0)), array_large[max(0, shift):min(len(array_large), len(array_small) + shift)], np.zeros(max(0, shift-length_diff))), axis=None) + array_small
-
-        shifted_abs_sum = np.sum(np.absolute(mixed_array))
-        shifted_abs_sum = shifted_abs_sum
-
-        if shifted_abs_sum > max_abs_sum:
-            max_abs_sum = shifted_abs_sum
-            optimal_shift = shift
-            optimal_mixed_array = mixed_array
-
-    print("{} : {}".format(file_large, optimal_shift/16000), flush=True)
-    sf.write(output_file, optimal_mixed_array, 16000)
+            if shifted_avg_sum > max_agv_sum:
+                max_agv_sum         = shifted_avg_sum
+                optimal_shift       = shift
+                optimal_mixed_array = mixed_array
+        sf.write(output_file, optimal_mixed_array, 16000)
+        return (input_files[0], input_files[1], max(0, optimal_shift), max(0, -optimal_shift))
 
 
 def mix_part3():
@@ -87,11 +60,9 @@ def mix_part3():
     wav_files = glob(os.path.join(root, '*.wav'), recursive=True)
     wav_files.sort()
 
-    
+
     params = []
     for key, value in groupby(wav_files, key=get_key):
-        if os.path.exists(os.path.join(root_out, f"{key}.wav")):
-            continue
 
         try:
             audio_file1, audio_file2 = [os.path.basename(file) for file in list(value)]
@@ -100,18 +71,20 @@ def mix_part3():
             continue
 
         wav_time1    = duration_dict["Audio_Separate"][audio_file1]
-        script_time1 = duration_dict["Scripts_Separate"][audio_file1.split(".")[0]+".TextGrid"]
         wav_time2    = duration_dict["Audio_Separate"][audio_file2]
-        script_time2 = duration_dict["Scripts_Separate"][audio_file2.split(".")[0]+".TextGrid"]
 
         if wav_time1 == wav_time2:
-            params.append(([os.path.join(root, audio_file1), os.path.join(root, audio_file2)], os.path.join(root_out, f"{key}.wav")))
+            params.append(([os.path.join(root, audio_file1), os.path.join(root, audio_file2)], os.path.join(root_out, f"{key}.wav"), "same_time"))
         else:
-            logging.error(f"Audio_Separate {key} || audio_file1:{int(wav_time1)}:{int(script_time1)}, audio_file2: {int(wav_time2)}:{int(script_time2)}")
+            params.append(([os.path.join(root, audio_file1), os.path.join(root, audio_file2)], os.path.join(root_out, f"{key}.wav"), "diff_time"))
 
+    shift_dict = defaultdict(float)
     with Pool(processes=32) as pool:
         results = list(tqdm(pool.imap_unordered(mix_wav, params), total=len(params)))
-        print(len(params), len(results), flush=True)
+        for file1, file2, shift1, shift2 in results:
+            shift_dict[file1.replace(".wav", ".TextGrid")].update(shift1)
+            shift_dict[file2.replace(".wav", ".TextGrid")].update(shift2)
+        json.dump(shift_dict, open("/scratch/users/astar/ares/zoux/workspaces/data_process/_data_in_processing/imda/imda_raw/PART3/mix_shift.json", "w"), indent=4)
 
 
 def mix_part4():
